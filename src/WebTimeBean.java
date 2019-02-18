f8f440de58e02c73d376c7557f6e952c2a624a1c
brianhighnam <brianhighnam@brians-mbp.fios-router.home>
2019-02-18 17:58:49 -0500

    intial commit

---
+++ src/WebTimeBean.java
@@ -0,0 +1,12 @@
+import java.text.DateFormat;
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean( name="webTimeBean" )
+public class WebTimeBean {
+	
+	public String getTime() {
+		return DateFormat.getTimeInstance( DateFormat.LONG ).format( new Date() );
+	}
+
+}
