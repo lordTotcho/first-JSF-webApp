0cdb880175ab0f7dc11eb72f55b4c94df5d477a3
brianhighnam <brianhighnam@192.168.1.154>
2019-02-20 10:21:29 -0500

    adding in comments

--- src/WebTimeBean.java
+++ src/WebTimeBean.java
@@ -2,6 +2,7 @@ import java.text.DateFormat;
 import java.util.Date;
 import javax.faces.bean.ManagedBean;
 
+// Indicate that the JSF framework should create and manage the webTimeBean objects
 @ManagedBean( name="webTimeBean" )
 public class WebTimeBean {
 	
